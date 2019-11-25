cb7ac19b858be6fcaf5c3f86abbce68dfa3cf1b0
Ming <lumingabby@outlook.com>
2019-11-24 21:42:27 -0500

    update

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@ rm -rf bin
 mkdir bin
 
 # complie java
-javac *.java -cp ./json-simple-1.1.1.jar -Xlint:unchecked -d ./bin
+javac ./src/*.java -cp ./json-simple-1.1.1.jar -Xlint:unchecked -d ./bin
 
 # cp shell and library
 cp run.sh bin/
 cp json-simple-1.1.1.jar bin/
 
 echo Done!
-exit 0
