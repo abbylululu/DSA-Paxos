c4cb35d238dd3e21d1326aec3099bde0403c5e7a
Jade Wang <wangyujue19960222@gmail.com>
2019-11-25 01:19:07 -0500

    [Add] Learner

--- src/ReservationSys.java
+++ src/ReservationSys.java
@@ -7,8 +7,8 @@ public class ReservationSys {
     // ------------------MEMBER VARS------------------ //
     private int uid;
     private ArrayList<HashMap<String, String>> sitesInfo;
-    private ArrayList<Reservation> dictionary; // local reservation data structure
-    private HashMap<Integer, String> log; // array of reservation string(ops clientName 1 2 3), in stable storage
+//    private ArrayList<Reservation> dictionary; // local reservation data structure
+//    private HashMap<Integer, String> log; // array of reservation string(ops clientName 1 2 3), in stable storage
     private Proposer proposer;
 
 
@@ -16,8 +16,8 @@ public class ReservationSys {
     public ReservationSys(ArrayList<HashMap<String, String>> sitesInfo, int uid, DatagramSocket sendSocket, BlockingQueue queue) {
         this.uid = uid;
         this.sitesInfo = sitesInfo;
-        this.dictionary = new ArrayList<>();
-        this.log = new HashMap<>();
+//        this.dictionary = new ArrayList<>();
+//        this.log = new HashMap<>();
         this.proposer = new Proposer(uid, this.sitesInfo, sendSocket, queue);
     }
 
@@ -27,9 +27,9 @@ public class ReservationSys {
     public int insert (String[] orderInfo) {
         // get the maxSlot index of current log
         int maxSlot = -1;
-        if (!this.log.isEmpty()) {
+        if (!Learner.log.isEmpty()) {
             // offset potential holes
-            for (Map.Entry<Integer, String> mapElement: this.log.entrySet()) {
+            for (Map.Entry<Integer, String> mapElement: Learner.log.entrySet()) {
                 maxSlot = Math.max(maxSlot, mapElement.getKey());
             }
 
@@ -60,9 +60,9 @@ public class ReservationSys {
     public int delete (String[] orderInfo) {
         // get the maxSlot index of current log
         int maxSlot = -1;
-        if (!this.log.isEmpty()) {
+        if (!Learner.log.isEmpty()) {
             // offset hole in log
-            for (Map.Entry<Integer, String> mapElement : this.log.entrySet()) {
+            for (Map.Entry<Integer, String> mapElement : Learner.log.entrySet()) {
                 maxSlot = Math.max(maxSlot, mapElement.getKey());
             }
 
@@ -76,7 +76,7 @@ public class ReservationSys {
         String cancel = "cancel " + clientName;
 
         // 2. check whether it has been deleted before
-        for (Map.Entry<Integer, String> mapElement: this.log.entrySet()) {
+        for (Map.Entry<Integer, String> mapElement: Learner.log.entrySet()) {
             if (cancel.equals(mapElement.getValue())) {
                 // cancel request failed
                 return -1;
@@ -91,33 +91,51 @@ public class ReservationSys {
     public boolean isConflict(ArrayList<Integer> flights) {
         // keep track of reserved flights in dict, mapping flight number to count
         HashMap<Integer, Integer> ReservedFlights = new HashMap<>();
-
-        for (int i = 0; i < dictionary.size(); i++) {
-            // reserved flights for each reservation provided in local dictionary
-            ArrayList<Integer> curLocalReservedFlights = dictionary.get(i).getFlights();
+        if (Learner.dictionary.isEmpty()) return false;
+        for (int i = 0; i < Learner.dictionary.size(); i++) {
+            ArrayList<Integer> curLocalReservedFlights = Learner.dictionary.get(i).getFlights();
             for (int j = 0; j < curLocalReservedFlights.size(); j++) {
                 Integer curFlight = curLocalReservedFlights.get(j);
-                // update current flight counts in dictionary
-                Integer curCnt = ReservedFlights.get(curFlight); // current flight counts in dictionary
-                ReservedFlights.put(curFlight, (curCnt == null) ? 1 : curCnt + 1);
+                if (ReservedFlights.containsKey(curFlight)) {
+                    Integer curCnt = ReservedFlights.get(curFlight);
+                    ReservedFlights.put(curFlight, curCnt + 1);
+                } else {
+                    ReservedFlights.put(curFlight, 1);
+                }
             }
         }
 
         for (int i = 0; i < flights.size(); i++) {
-            int flight = flights.get(i);
-            // if there exists a flight that is fully booked in log, that is a conflict
-            if (!ReservedFlights.isEmpty() && ReservedFlights.get(flight) != null) {
-                if (ReservedFlights.get(flight) == 2) {
-                    return true;
-                }
+            if (ReservedFlights.containsKey(flights.get(i)) && ReservedFlights.get(flights.get(i)) == 2) {
+                return true;
             }
         }
+//        for (int i = 0; i < Learner.dictionary.size(); i++) {
+//            // reserved flights for each reservation provided in local dictionary
+//            ArrayList<Integer> curLocalReservedFlights = Learner.dictionary.get(i).getFlights();
+//            for (int j = 0; j < curLocalReservedFlights.size(); j++) {
+//                Integer curFlight = curLocalReservedFlights.get(j);
+//                // update current flight counts in dictionary
+//                Integer curCnt = ReservedFlights.get(curFlight); // current flight counts in dictionary
+//                ReservedFlights.put(curFlight, (curCnt == null) ? 1 : curCnt + 1);
+//            }
+//        }
+//
+//        for (int i = 0; i < flights.size(); i++) {
+//            int flight = flights.get(i);
+//            // if there exists a flight that is fully booked in log, that is a conflict
+//            if (!ReservedFlights.isEmpty() && ReservedFlights.get(flight) != null) {
+//                if (ReservedFlights.get(flight) == 2) {
+//                    return true;
+//                }
+//            }
+//        }
         return false;
     }
 
 
     public void updateDict() {
-        for (Map.Entry<Integer, String> mapElement: this.log.entrySet()) {
+        for (Map.Entry<Integer, String> mapElement: Learner.log.entrySet()) {
             if (mapElement.getValue() == null) continue;
             String curEntry = mapElement.getValue();
 
@@ -137,28 +155,28 @@ public class ReservationSys {
 //                if (!this.dictionary.isEmpty()) System.out.println("####in dict: " + this.dictionary.get(0).flatten());
 //                System.out.println("&&&&&&" + curResvObj.equals(this.dictionary.get(0)));
                 boolean dups = false;
-                for (int i = 0; i < this.dictionary.size(); i++) {
-                    if (this.dictionary.get(i).flatten().equals(curEntry)) {
+                for (int i = 0; i < Learner.dictionary.size(); i++) {
+                    if (Learner.dictionary.get(i).flatten().equals(curEntry)) {
                         dups = true;
                     }
                 }
                 if (dups) continue;
 //                if (this.dictionary.contains(curResvObj)) continue;
 //                System.out.println("***&&&");
-                this.dictionary.add(curResvObj);
+                Learner.dictionary.add(curResvObj);
             }
             // delete
             else if (operation.equals("cancel")) {
                 String clientName = splitted[1];
                 System.out.println("***log entry cancle client: " + clientName);
 
-                for(int i = 0; i < this.dictionary.size(); i++) {
-                    if (this.dictionary.get(i).getClientName().equals(clientName)) {
-                        System.out.println("***deleting record: " + this.dictionary.get(i).flatten());
+                for(int i = 0; i < Learner.dictionary.size(); i++) {
+                    if (Learner.dictionary.get(i).getClientName().equals(clientName)) {
+                        System.out.println("***deleting record: " + Learner.dictionary.get(i).flatten());
 
-                        this.dictionary.remove(this.dictionary.get(i));
+                        Learner.dictionary.remove(Learner.dictionary.get(i));
 
-                        System.out.println("***size after: " + this.dictionary.size());
+                        System.out.println("***size after: " + Learner.dictionary.size());
                     }
                 }
             }
@@ -170,7 +188,7 @@ public class ReservationSys {
         for (int i = 0; i <= maxSlot; i++) {
             // TODO: check key first then value next
             // there is a hole in local log
-            if (this.log.get(i) == null) {
+            if (Learner.log.get(i) == null) {
                 System.out.println("hello????");
                 int cnt = 3;
                 while (cnt > 0) {
@@ -185,7 +203,7 @@ public class ReservationSys {
                 }
                 // successfully learnt hole
                 if (cnt > 0) {
-                    this.log.put(i, proposer.getMaxVal());
+                    Learner.log.put(i, proposer.getMaxVal());
                     this.store();
                     // update local dict
                     this.updateDict();
@@ -213,7 +231,7 @@ public class ReservationSys {
             else if(proposeRet == -1) {
                 System.out.println("hello???????");
                 // store information for current chosen slot
-                this.log.put(this.proposer.getNext_log_slot(), proposer.getMaxVal());
+                Learner.log.put(this.proposer.getNext_log_slot(), proposer.getMaxVal());
                 this.store();
                 // update local dict
                 this.updateDict();
@@ -225,7 +243,7 @@ public class ReservationSys {
         // successfully proposed
         if (cnt > 0) {
             // update log entry
-            this.log = this.proposer.learn(this.log);
+            Learner.log = this.proposer.learn(Learner.log);
             this.store();
             // update local dict
             this.updateDict();
@@ -241,7 +259,7 @@ public class ReservationSys {
     // format: slot,ops clientName (1 2 3)
     public void store() {
         // logSlot accVal senderIp
-        for (Map.Entry<Integer, String> mapElement: this.log.entrySet()) {
+        for (Map.Entry<Integer, String> mapElement: Learner.log.entrySet()) {
             // empty log slot
             if (mapElement.getValue() == null) continue;
             String log = mapElement.getKey() + "," + mapElement.getValue();
@@ -264,21 +282,21 @@ public class ReservationSys {
             String[] splitted = line.split(",");
             int curSlot = Integer.parseInt(splitted[0]);
             String curLogEntry = splitted[1];
-            this.log.put(curSlot, curLogEntry);
+            Learner.log.put(curSlot, curLogEntry);
         }
         this.updateDict();
     }
 
 
     void printLog() {
-        for (int i = 0; i < this.log.size(); i++) {
-            System.out.println(this.log.get(i));
+        for (int i = 0; i < Learner.log.size(); i++) {
+            System.out.println(Learner.log.get(i));
         }
     }
 
     void printDictionary() {
         ArrayList<Reservation> newDict = new ArrayList<>();
-        newDict = this.dictionary;
+        newDict = Learner.dictionary;
         for (int i = 0; i < newDict.size(); i++) {
             newDict.sort(new CustomComparator());
             System.out.println(newDict.get(i).flatten());
@@ -304,19 +322,19 @@ public class ReservationSys {
     }
 
     public ArrayList<Reservation> getDictionary() {
-        return dictionary;
+        return Learner.dictionary;
     }
 
     public void setDictionary(ArrayList<Reservation> dictionary) {
-        this.dictionary = dictionary;
+        Learner.dictionary = dictionary;
     }
 
     public HashMap<Integer, String> getLog() {
-        return log;
+        return Learner.log;
     }
 
     public void setLog(HashMap<Integer, String> log) {
-        this.log = log;
+        Learner.log = log;
     }
 
     public Proposer getProposer() {
